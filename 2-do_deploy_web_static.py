4dd2c102f3d2dc59c54c5dae4f4d0d1cb287dbfd
Emma0147 <mromotoriogunzabere@gmail.com>
2023-04-06 15:17:48 -0700

    All Task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.26.210.109', '54.160.70.225']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
